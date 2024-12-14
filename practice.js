45023ebcda5e7fc9e6f719021b6ad62527f10e00
Ahmad Abdul Hamid  Younes <96975157+AYOUNS@users.noreply.github.com>
2024-12-15 00:13:06 +0200

    new add

---
+++ practice.js
@@ -0,0 +1,46 @@
+// "use strict";
+// document.querySelector(".message").textContent = "🎉 Correct Number!";
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+// document.querySelector("guess").value;
+// document.querySelector("guess").value = 23;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  console.log(typeof guess);
+  //when there is no input
+  if (!guess) {
+    document.querySelector(".message").textContent = "♦️ No Number";
+  }
+  //when player wins
+  else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 correct Number";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+  }
+  // when guess is too high
+  else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "☝️ too high";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 you lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+    // when guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "👇 too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 you lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
