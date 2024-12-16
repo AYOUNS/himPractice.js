1297e7d49706d74dd88f2284edf3a54f6dcafc40
Ahmad Abdul Hamid  Younes <96975157+AYOUNS@users.noreply.github.com>
2024-12-16 23:51:03 +0200

    new

--- practice.js
+++ practice.js
@@ -7,7 +7,7 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
@@ -19,6 +19,8 @@ document.querySelector(".check").addEventListener("click", function () {
   //when player wins
   else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 correct Number";
+    document.querySelector(".number").textContent = secretNumber;
+
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
   }
